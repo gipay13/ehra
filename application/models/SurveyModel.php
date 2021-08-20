96d1cfe7de388ec503e250b5574f3da003638959
ariestiand <ghifariansar13@gmail.com>
2021-08-20 20:43:32 +0700

    Ubah lagi

--- application/models/SurveyModel.php
+++ application/models/SurveyModel.php
@@ -3,11 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class SurveyModel extends CI_Model
 {
-	function insertJawaban($no_survey, $id_pertanyaan, $jawaban) {
+	function insertJawaban($no_survey, $id_pertanyaan, $jawaban, $ket) {
 		$answer = array(
 			'id_survey'		=> $no_survey,
 			'id_pertanyaan' => $id_pertanyaan,
-			'jawaban' 		=> $jawaban
+			'jawaban' 		=> $jawaban,
+			'ket'			=> $ket,
 		);
 
 		$this->db->insert('hasil_survey', $answer);

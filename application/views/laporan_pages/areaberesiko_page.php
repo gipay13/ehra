be72352e67900f8185963cc94885591c77ee166c
ariestiand <ghifariansar13@gmail.com>
2021-10-27 15:54:33 +0700

    laporan

--- application/views/laporan_pages/areaberesiko_page.php
+++ application/views/laporan_pages/areaberesiko_page.php
@@ -10,8 +10,50 @@
 <section class="content">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-12 mb-5">
-				<div id="banjir" style="height: 370px;"></div>
+			<div class="col-md-12">
+				<div class="card">
+					<form action="<?= base_url('laporan/export') ?>" method="post">
+					<div class="card-header">
+						Laporan
+					</div>
+					<div class="card-body">
+						<div class="form-group">
+							<label for="username">Nama Laporan</label>
+							<select name="laporan" id="laporan" class="form-control">
+								<option value="sabe">Sumber Air Beresiko</option>
+								<option value="ald">Air Limbah Domestik</option>
+								<option value="per">Persampahan</option>
+								<option value="ga">Genangan Air</option>
+								<option value="phbs">Perilaku Hidup Bersih Sehat</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<label for="username">Tanggal</label>
+							<div class="row">
+								<div class="col-md-5 col-sm-12">
+									<input type="date" name="initial_date" id="initial_date" class="form-control" value="<?= date('Y-m-d') ?>">
+								</div>
+								<div class="col-md-2 col-sm-12">
+									<p class="text-center my-2">to</p>
+								</div>
+								<div class="col-md-5 col-sm-12">
+									<input type="date" name="end_date" id="end_date" class="form-control" value="<?= date('Y-m-d') ?>">
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="username">Wilayah</label>
+							<select name="wilayah" id="wilayah" class="form-control">
+								<option value="kecamatan">Kecamatan</option>
+								<option value="kelurahan">Kelurahan</option>
+							</select>
+						</div>		
+					</div>
+					<div class="card-footer">
+						<button type="submit" class="btn btn-danger"><i class="fas fa-file-pdf"></i> Export Laporan</button>
+					</div>
+					</form>	
+				</div>
 			</div>
 		</div>
 	</div>
